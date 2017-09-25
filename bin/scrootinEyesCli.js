51e11a9ec570ca8d79339a19e7fbc5cc00d145fc
srsheldon <samuelrsheldon@gmail.com>
2017-09-24 23:03:21 -0400

    Added string literal welcome banner and started to create custom logger

--- bin/scrootinEyesCli.js
+++ bin/scrootinEyesCli.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
 'use strict';
+//String literal of the scrootinEyes logo for welcoming the user 🤗
+const welcomeBanner = '\
+                           _   _       ______                     \n \
+                         | | (_)     |  ____|                     \n \
+ ___  ___ _ __ ___   ___ | |_ _ _ __ | |__  _   _  ___  ___       \n \
+/ __|/ __| \'__/   \\ /   \\| __| | \'_ \\|  __|| | | |/ _ \\/ __|\n \
+\\__ \\ (__| | |   * |   * | |_| | | | | |___| |_| |  __/\\__ \\  \n \
+|___/\\___|_| /\\___/ \\___/ \\__|_|_| |_|______\\__, |\\___||___/\n \
+            //                              __/ /                 \n \
+           //                              |___/                  \n\n';
+
+
+ console.log(welcomeBanner);
