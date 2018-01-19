e508674bc60d5e2258539f7fd44a9aa68e958b66
srsheldon <samuelrsheldon@gmail.com>
2018-01-19 01:21:35 -0500

    testing out the git hub pages

--- test/scrootinEyesCli.test.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-import scrootinEyesCli from '../bin/scrootinEyesCli';
-import assert from 'assert';
-
-assert.deepStrictEqual(scrootinEyesCli.SCROOTINEYES_LOGO_STRING_LITERAL.length, 528);
