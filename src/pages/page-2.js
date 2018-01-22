ba966ac2082af093d83a1f8ca7437a1527b2b6d5
srsheldon <samuelrsheldon@gmail.com>
2018-01-22 06:06:26 -0500

    :sparkles: Add gatsby static site, made small updates

---
+++ src/pages/page-2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div className="container">
+    <h2>This page is just a test</h2>
+    <p>experimenting with gatsby-link</p>
+  </div>
+)
+
+export default SecondPage
